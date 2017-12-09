312f6fa6bcaa7af5fd0c185e37ebf5a4a52dd7d3
LeoAlioth <anzek.kosir@gmail.com>
2017-12-09 09:47:05 +0100

    dates

--- Script.R
+++ Script.R
@@ -1,1 +1,4 @@
 data = read.table("podatkiSem1.txt", header = TRUE, sep = ",");
+data$Datum = as.Date(data$Datum);
+ucnamnozica = data[data$Datum <= as.Date("2014-12-31")];
+testnamnozica = data[data$Datum > as.Date("2014-12-31")];
