3cfc389a7b756126c5d32e39ea22ed8c7d101c2d
thechosenone786 <34382595+thechosenone786@users.noreply.github.com>
2017-12-09 10:37:31 +0100

    fix

--- Script.R
+++ Script.R
@@ -1,4 +1,5 @@
 data = read.table("podatkiSem1.txt", header = TRUE, sep = ",");
 data$Datum = as.Date(data$Datum);
-ucnamnozica = data[data$Datum <= as.Date("2014-12-31")];
-testnamnozica = data[data$Datum > as.Date("2014-12-31")];
+ucnamnozica = data[data$Datum <= as.Date("2014-12-31"),];
+testnamnozica = data[data$Datum > as.Date("2014-12-31"),];
+
